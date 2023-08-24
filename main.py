1a94f939d0f0865e559f09ea7cbb1e5c8a140135
Lesha <simonovalex2017@gmail.com>
2023-08-24 17:08:00 +0300

    added players

--- main.py
+++ main.py
@@ -18,7 +18,26 @@ class GameObject(pygame.sprite.Sprite):
         self.speed = speed
     def update(self):
         window.blit(self.image, (self.rect.x  ,self.rect.y ))
+class Player(GameObject):
+    def __init__(self, x, y, w, h, image , speed , ):
+        super().__init__(x, y, w, h, image , speed)
+    def move(self,K_UP,K_DOWN):
+        k = pygame.key.get_pressed()
+        if k[K_UP]:
+            if  self.rect.y >= 0:
+                self.rect.y -= self.speed
+            
+        if k[K_DOWN]:
+            if self.rect.y <= 600-self.rect.h :
+                self.rect.y += self.speed     
+player1 = Player(55,win_h/2 , 25,115,platform_pict ,4 )  
+player2 = Player(615,win_h/2 , 25,115,platform_pict ,4 )  
 while game :
+    window.fill(win_fon)
+    player1.update()
+    player2.update()
+    player1.move(pygame.K_w , pygame.K_s)
+    player2.move(pygame.K_UP , pygame.K_DOWN)
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game = False
